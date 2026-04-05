bb3e94326dbed9bf6c56f713eb21e2875fa74249
Robin Michael <Siliconrob@siliconheaven.net>
2026-04-05 16:41:32 -0500

    Update test_records.py

--- tests/test_records.py
+++ tests/test_records.py
@@ -5,6 +5,9 @@ from pydantic import SecretStr
 
 from RecordTypes.Credentials import Credentials
 from rich.console import Console
+
+from main import get_connection_params
+
 console = Console()
 
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
@@ -13,6 +16,11 @@ IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 def test_credentials_input():
     creds = Credentials(user_name="user", password=SecretStr("password"))
     assert str(creds.password) == '**********'
+
+
+def test_get_connect_params():
+    z = get_connection_params()
+    assert z.get("account") is not None
 
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
